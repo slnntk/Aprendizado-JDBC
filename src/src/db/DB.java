9e912622eceb0ee1fc66701226cfb0b1703b595f
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-28 14:11:31 -0300

    Update DB.java

--- src/src/db/DB.java
+++ src/src/db/DB.java
@@ -1,4 +1,54 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DB{
+
+    private static Connection conn = null;
+
+    private static Connection getConnection(){
+        //Testar se o objeto Connection (conn) continua nulo, se continuar a connection ainda não foi feita.
+        if (conn == null){
+            try {
+                // Criar um objeto props que vai chamar a função load properties para justamente receber todas as propriedades necessarias para a connection
+                Properties props = loadProperties();
+                // String url vai receber a url, o props.getProperty funciona como um map, então é enviado o valor da key e é recebido o valor
+                String url = props.getProperty("dburl");
+                // Agora é finalmente instanciar connection com a coneção, usando o driver
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+
+        }
+        return conn;
+    }
+
+    public static void closeConnection(){
+        if (conn != null){
+            try {
+               conn.close();
+            } catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties(){
+        // Abrir o arquivo db.properties quje está na pasta do projeto.
+        try (FileInputStream fs = new FileInputStream("db.properties")){
+            // Criar o objeto properties que ficara responsavel por guardar as informações das propriedas do banco.
+            Properties ps = new Properties();
+            // Carregar as propriedades que estão no nosso arquivo para o objeto Properties
+            ps.load(fs);
+            return ps;
+        } catch (IOException e) {
+            throw new DbException("Error in load the properties" + e.getMessage());
+        }
+    }
 }
