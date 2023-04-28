ba7e3e85d9f4eca2064b62ff0cf7e9a1094da925
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-28 13:55:48 -0300

    -

---
+++ src/src/jbdbc1/DbException.java
@@ -0,0 +1,12 @@
+package jbdbc1;
+
+import java.io.Serial;
+
+public class DbException extends RuntimeException{
+    @Serial
+    private  static final long serialVersionUID = 1L;
+
+    public DbException(String message) {
+        super(message);
+    }
+}
