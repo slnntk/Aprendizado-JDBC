36a7e637f0bbd10b7a36095f7fe8551e6d557682
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-28 15:13:04 -0300

    nice !!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,37 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        // Conectar com BD
+        Connection conn = null;
+        // Preparar uma consulta SQL para buscar todos os dados que quero.
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            // Executa a busca procurando todos os elementos de department e guarda em st
+            st.executeQuery("select * from department");
+            // ResultSet recebido de st.getResultSet();
+            rs = st.getResultSet();
+
+            // Percorrer rs.next enquanto for verdadeiro (quando chegar no final ele da false)
+            while (rs.next()){
+                //pegar o inteiro que está no campo "Id", pegar a string que está no campo name
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
