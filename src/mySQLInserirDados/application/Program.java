e4721904f2add0ee6045b19f191770e8161e0293
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-29 15:15:54 -0300

    continue with de insert data, print the id of the row affected

--- src/mySQLInserirDados/application/Program.java
+++ src/mySQLInserirDados/application/Program.java
@@ -24,15 +24,26 @@ public class Program {
                         "INSERT INTO seller "
                        +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                        +"VALUES "
-                       +"(?, ?, ?, ?, ?)");
+                       +"(?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
                 st.setString(1, "Carl Purple");
                 st.setString(2, "carl@gmail.com");
                 st.setDate(3, new java.sql.Date(sdf.parse("16/08/2002").getTime()));
                 st.setDouble(4, 3000.0);
                 st.setInt(5, 4);
-                
+
                 int rowsAffected = st.executeUpdate();
-                System.out.println("Done! Rows affected: " + rowsAffected);
+
+                if (rowsAffected > 0){
+                    ResultSet rs = st.getGeneratedKeys();
+                    while (rs.next()){
+                        int id = rs.getInt(1);
+                        System.out.println("Done| Id = " + id);
+                    }
+                } else{
+                    System.out.println("No rows affected! ");
+                }
+
             } catch (ParseException | SQLException e) {
                 throw new DbException(e.getMessage());
             }
