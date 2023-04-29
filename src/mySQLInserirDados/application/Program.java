1c54385f4c24c1e8106f4b0ca45dc6f27061a4d8
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-29 15:35:44 -0300

    insert in DB passing values

--- src/mySQLInserirDados/application/Program.java
+++ src/mySQLInserirDados/application/Program.java
@@ -16,37 +16,37 @@ public class Program {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
-
             conn = DB.getConnection();
-            // Receber o comando para inserção de dados
-            try {
-                st = conn.prepareStatement(
-                        "INSERT INTO seller "
-                       +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                       +"VALUES "
-                       +"(?, ?, ?, ?, ?)",
-                Statement.RETURN_GENERATED_KEYS);
-                st.setString(1, "Carl Purple");
-                st.setString(2, "carl@gmail.com");
-                st.setDate(3, new java.sql.Date(sdf.parse("16/08/2002").getTime()));
-                st.setDouble(4, 3000.0);
-                st.setInt(5, 4);
 
-                int rowsAffected = st.executeUpdate();
+           /*
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                   +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                   +"VALUES "
+                   +"(?, ?, ?, ?, ?)",
+            Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("16/08/2002").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);*/
 
-                if (rowsAffected > 0){
-                    ResultSet rs = st.getGeneratedKeys();
-                    while (rs.next()){
-                        int id = rs.getInt(1);
-                        System.out.println("Done| Id = " + id);
-                    }
-                } else{
-                    System.out.println("No rows affected! ");
+            st = conn.prepareStatement("insert into department (Name) values ('D1'), ('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done| Id = " + id);
                 }
-
-            } catch (ParseException | SQLException e) {
-                throw new DbException(e.getMessage());
+            } else {
+                System.out.println("No rows affected! ");
             }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
