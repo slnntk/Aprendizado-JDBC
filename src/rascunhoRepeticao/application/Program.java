fad06f29da52c8f989895a99ee27eb23d42db9a4
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-01 17:53:32 -0300

    recovery data !
    recuperar dados de um BC

--- src/rascunhoRepeticao/application/Program.java
+++ src/rascunhoRepeticao/application/Program.java
@@ -1,8 +1,8 @@
 package rascunhoRepeticao.application;
 
 
-import rascunhoRepeticao.db.DB;
 import rascunhoRepeticao.db.DbException;
+import rascunhoRepeticao.db.DB;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,6 +12,23 @@ import java.sql.Statement;
 public class Program {
     public static void main(String[] args) {
 
-        
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from department");
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            throw new DbException("Error");
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
     }
 }
