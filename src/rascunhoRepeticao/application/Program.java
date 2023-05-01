efd4be3fea53c488aa4b5111c6620d165a148d46
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-01 18:40:35 -0300

    Update Program.java

--- src/rascunhoRepeticao/application/Program.java
+++ src/rascunhoRepeticao/application/Program.java
@@ -4,31 +4,30 @@ package rascunhoRepeticao.application;
 import rascunhoRepeticao.db.DbException;
 import rascunhoRepeticao.db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Program {
     public static void main(String[] args) {
 
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
 
-        try {
+        try{
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
-            while (rs.next()){
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
-        } catch (SQLException e) {
-            throw new DbException("Error");
-        } finally {
-            DB.closeStatement(st);
-            DB.closeResultSet(rs);
-            DB.closeConnection();
+
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, Birthday, BaseSalary, DepartmentId)"
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)");
+            st.setString(1, "Name");
+            st.setString(2, "Email");
+            st.setString(3, "Birth");
+            st.setString(4, "Email");
+            st.setString(5, "Email");
+            )
         }
+
+
     }
 }
