0c92b72f3fbe50bf06e024e2b3433711b14213dd
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-02 12:13:49 -0300

    Update Program.java

--- src/rascunhoRepeticao/application/Program.java
+++ src/rascunhoRepeticao/application/Program.java
@@ -7,27 +7,16 @@ import rascunhoRepeticao.db.DB;
 import java.sql.*;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Connection conn = null;
         PreparedStatement st = null;
+        conn = DB.getConnection();
 
-        try{
-            conn = DB.getConnection();
-
-            st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(Name, Email, Birthday, BaseSalary, DepartmentId)"
-                    + "VALUES "
-                    + "(?, ?, ?, ?, ?)");
-            st.setString(1, "Name");
-            st.setString(2, "Email");
-            st.setString(3, "Birth");
-            st.setString(4, "Email");
-            st.setString(5, "Email");
-            )
-        }
-
-
+        st = conn.prepareStatement(
+                "INSERT INTO seller "
+                + "(Name, Email, Birthday, BaseSalary, DepartmentId)"
+                + "VALUES "
+                + "(?, ?, ?, ?, ?)");
     }
 }
